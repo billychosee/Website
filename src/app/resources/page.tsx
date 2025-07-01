fca71b6a98e2d16d6c8298da6d13159aceb83ffc
billychosee <billychosee@gmail.com>
2025-07-01 15:25:20 +0200

    Final Changes ts configgfdsd

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Resources() {
+  return (
+    <div className='bg-white py-24 text-black text-center'>
+      <h1 className='text-5xl font-bold'>Resources</h1>
+    </div>
+  )
+}
+
+export default Resources
