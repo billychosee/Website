f6a4c5b50917da3b36cdb0b14532eea8e798cc11
billychosee <billychosee@gmail.com>
2025-07-03 12:19:47 +0200

    Final Changes ts configgfdsdhhffkg

--- src/app/components/ImageLeftAccordionRight.tsx
+++ src/app/components/ImageLeftAccordionRight.tsx
@@ -28,13 +28,13 @@ export function ImageLeftAccordionRight({
   return (
     <div className="flex flex-col items-center gap-8 px-5 py-8 md:flex-row md:px-24">
       {/* Image on the left */}
-      <div className="w-full md:w-1/2">
+      <div className="flex my-5 overflow-hidden md:w-1/2 h-96 rounded-xl">
         <Image
           src={imageSrc}
           alt={imageAlt}
           width={600}
           height={400}
-          className="rounded shadow"
+          className="object-cover w-full h-full rounded shadow"
         />
       </div>
 
@@ -44,7 +44,7 @@ export function ImageLeftAccordionRight({
           <div key={index} className="py-4 border-b border-gray-300">
             <button
               onClick={() => toggle(index)}
-              className="flex justify-between w-full text-left font-semibold text-[#4A90A4] hover:text-[#00b956] focus:outline-none"
+              className="flex justify-between w-full text-left font-semibold text-[#4A90A4] hover:text-[#00b956] focus:outline-none text-[16px] lg:text-[14px] xl:text-[16px]"
             >
               <span>{item.question}</span>
               <span className="text-xl">
